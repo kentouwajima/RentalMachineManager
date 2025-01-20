e0861705cde4290bd10898758d16c0b790a2b722
kentouwajima <u.kento6366@gmail.com>
2025-01-20 21:36:40 +0900

    Initial commit

---
+++ src/main/java/RentalMachineManager/RentalMachineManagerApplication.java
@@ -0,0 +1,13 @@
+package RentalMachineManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalMachineManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalMachineManagerApplication.class, args);
+	}
+
+}
