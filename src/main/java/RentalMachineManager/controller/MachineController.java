14606054addbdd670744739db9674df3a9aaeb4b
kentouwajima <u.kento6366@gmail.com>
2025-01-21 15:47:13 +0900

    @Autowiredをコンストラクタインジェクションとして使用

--- src/main/java/RentalMachineManager/controller/MachineController.java
+++ src/main/java/RentalMachineManager/controller/MachineController.java
@@ -3,6 +3,7 @@ package RentalMachineManager.controller;
 import RentalMachineManager.model.Machine;
 import RentalMachineManager.service.MachineService;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class MachineController {
   private final MachineService machineService;
 
+  @Autowired
   public MachineController(MachineService machineService) {
     this.machineService = machineService;
   }
--- src/main/java/RentalMachineManager/service/MachineService.java
+++ src/main/java/RentalMachineManager/service/MachineService.java
@@ -3,12 +3,14 @@ package RentalMachineManager.service;
 import RentalMachineManager.model.Machine;
 import RentalMachineManager.repository.MachineRepository;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MachineService {
   private final MachineRepository machineRepository;
 
+  @Autowired
   public MachineService(MachineRepository machineRepository) {
     this.machineRepository = machineRepository;
   }
