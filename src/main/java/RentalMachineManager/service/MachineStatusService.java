806f8879411bff088332a6f18f74d7f77c9a3e1c
kentouwajima <u.kento6366@gmail.com>
2025-01-27 08:50:45 +0900

    MachineStatusServiceのリファクタリング

--- src/main/java/RentalMachineManager/service/MachineStatusService.java
+++ src/main/java/RentalMachineManager/service/MachineStatusService.java
@@ -7,14 +7,18 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class MachineStatusService {
+
+  // MachineStatusRepositoryのインスタンスを注入
   private final MachineStatusRepository machineStatusRepository;
 
+  // コンストラクタでMachineStatusRepositoryを受け取り、フィールドにセット
   public MachineStatusService(MachineStatusRepository machineStatusRepository) {
     this.machineStatusRepository = machineStatusRepository;
   }
 
+  // すべての機材ステータス情報を取得するメソッド
   public List<MachineStatus> getAllStatuses() {
-    return machineStatusRepository.findAll();
+    return machineStatusRepository.findAll(); // MachineStatusRepositoryから機材ステータスのデータを取得
   }
 
 }
