8562d55176a4f834c05ba4f802b5ab62c81a6e7d
kentouwajima <u.kento6366@gmail.com>
2025-01-27 08:33:16 +0900

    MachineServiceのリファクタリング

--- src/main/java/RentalMachineManager/service/MachineService.java
+++ src/main/java/RentalMachineManager/service/MachineService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class MachineService {
+
   private final MachineRepository machineRepository;
 
   @Autowired
@@ -15,24 +16,29 @@ public class MachineService {
     this.machineRepository = machineRepository;
   }
 
+  // 全ての機材情報を取得
   public List<Machine> getAllMachines() {
-    return machineRepository.findAll();
+    return machineRepository.findAll(); // 機材一覧を取得する
   }
 
+  // 機材IDを指定して詳細情報を取得
   public Machine getMachineById(int id) {
     return machineRepository.findById(id); // 詳細情報を取得する
   }
 
+  // 新規機材の登録
   public void createMachine(Machine machine) {
-    machineRepository.insertMachine(machine);
+    machineRepository.insertMachine(machine); // 新規機材を追加する
   }
 
-  public void deleteMachine(int id) {
-    machineRepository.deleteById(id);
-  }
-
+  // 機材情報の更新
   public void updateMachine(Machine machine) {
     machineRepository.updateMachine(machine); // Repositoryでの更新処理を呼び出す
   }
 
+  // 機材の削除
+  public void deleteMachine(int id) {
+    machineRepository.deleteById(id); // 機材を削除する
+  }
+  
 }
